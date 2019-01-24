564f20a266f2c11d086fbc244ffc02091385609b
Tommyshere <tommyshere@gmail.com>
2019-01-23 23:17:38 -0800

    Bootstrap and Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module'
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
@@ -11,8 +14,11 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
+    NgbModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
